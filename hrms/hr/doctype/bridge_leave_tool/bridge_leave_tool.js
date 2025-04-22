5e1eb1dfaa961b991df8603933019a6372ffded0
Ovresko <ovresko@gmail.com>
2025-04-22 08:02:24 +0100

    bridge

--- hrms/hr/doctype/bridge_leave_tool/bridge_leave_tool.js
+++ hrms/hr/doctype/bridge_leave_tool/bridge_leave_tool.js
@@ -1,8 +1,53 @@
-// Copyright (c) 2025, Frappe Technologies Pvt. Ltd. and contributors
-// For license information, please see license.txt
 
 frappe.ui.form.on('Bridge leave tool', {
-	// refresh: function(frm) {
+	refresh(frm) {
+		frm.trigger("set_primary_action");
+	},
 
-	// }
+	
+	status(frm) {
+		frm.trigger("set_primary_action");
+	},
+
+	reset_attendance_fields(frm) {
+		frm.set_value("status", "");
+		frm.set_value("shift", "");
+		frm.set_value("late_entry", 0);
+		frm.set_value("early_exit", 0);
+	},
+
+
+
+	set_primary_action(frm) {
+		frm.disable_save();
+		frm.page.set_primary_action(__("Mark Leave"), () => {
+
+			frm.trigger("mark_leave");
+		});
+	},
+
+	mark_leave(frm) {
+
+		frappe
+			.call({
+				method: "hrms.hr.doctype.bridge_leave_tool.bridge_leave_tool.mark_employee_leave",
+				args: {
+					start_date: frm.doc.start_date,
+					end_date: frm.doc.end_date,
+					leave_type: frm.doc.leave_type,
+					reason: frm.doc.reason,
+				},
+				freeze: true,
+				freeze_message: __("Marking Leaves"),
+			})
+			.then((r) => {
+				if (!r.exc) {
+					frappe.show_alert({
+						message: __("Leaves marked successfully"),
+						indicator: "green",
+					});
+					frm.refresh();
+				}
+			});
+	},
 });
--- hrms/hr/doctype/bridge_leave_tool/bridge_leave_tool.py
+++ hrms/hr/doctype/bridge_leave_tool/bridge_leave_tool.py
@@ -1,8 +1,56 @@
 # Copyright (c) 2025, Frappe Technologies Pvt. Ltd. and contributors
 # For license information, please see license.txt
 
-# import frappe
+
+import datetime
+import json
+
+import frappe
 from frappe.model.document import Document
+from frappe.utils import getdate
 
 class Bridgeleavetool(Document):
 	pass
+
+
+@frappe.whitelist()
+def mark_employee_leave(
+	start_date: str | datetime.date,
+	end_date: str | datetime.date,
+	leave_type: str | None = None,
+	reason: str | None = None,
+) -> None:
+	if isinstance(start_date, str):
+		start_date = getdate(start_date)
+	if isinstance(end_date, str):
+		end_date = getdate(end_date)
+
+	if not isinstance(start_date, datetime.date) or not isinstance(end_date, datetime.date):
+		frappe.throw("Invalid date format for start_date or end_date.")
+
+	if start_date > end_date:
+		frappe.throw("Start date cannot be after end date.")
+
+	if not leave_type:
+		frappe.throw("Leave type must be specified.")
+
+	if not reason:
+		frappe.throw("Reason for leave must be specified.")
+
+	employee_list = frappe.get_list(
+		"Employee", fields=["employee", "employee_name"], filters={"status": "Active","name":"999999"}, order_by="employee_name"
+	)
+	for employee in employee_list:
+		attendance = frappe.get_doc(
+			dict(
+				doctype="Leave Application",
+				employee=employee,
+				from_date=start_date,
+				to_date=end_date,
+				leave_type=leave_type,
+				reason=reason,
+				status="Approved",
+			)
+		)
+		attendance.insert()
+		attendance.submit()
